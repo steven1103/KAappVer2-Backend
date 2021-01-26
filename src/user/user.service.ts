7bb96fa0cb26554cdd39194c6c63e5fed8319da0
steven1103 <steven1103@naver.com>
2021-01-26 11:49:49 +0900

    first function 🔧

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private readonly users: Repository<User>,
+  ) {}
+
+  async getUser({ username }): Promise<{ user?: User; ok: boolean }> {
+    const exist = await this.users.findOne({ username });
+    if (!exist) {
+      return {
+        ok: false,
+      };
+    }
+    return {
+      user: exist,
+      ok: true,
+    };
+  }
+}
